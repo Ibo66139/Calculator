ebcc96a6848054596e63dfbcaf3abd6e888d9c73
Ibrahim kocak <ibo66kocak@gmail.com>
2024-02-26 00:26:52 +0100

    Add negative numbers

--- script.js
+++ script.js
@@ -12,8 +12,15 @@ function calculate() {
     con.innerHTML = result;
 }
 
-
 function deleteLast() {
     let con = document.getElementsByClassName('result')[0];
     con.innerHTML = con.innerHTML.slice(0, -1);
+}
+
+function toggleSign() {
+    let display = document.getElementsByClassName('result')[0];
+    let currentValue = parseFloat(display.innerHTML);
+    if (!isNaN(currentValue)) {
+        display.innerHTML = -currentValue;
+    }
 }
